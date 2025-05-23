7a1875a18d7df3233629cc9451258fc39ca50462
Yousra0225 <chbibyousra02@gmail.com>
2025-05-23 15:51:24 +0200

    feat(main) add main class

--- code/corpo-event/src/Main.java
+++ code/corpo-event/src/Main.java
@@ -2,6 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        CollaboratorService service = new CollaboratorService();
+        Collaborator c1 = new Collaborator("Ali", "Baba", "ali@mail.com", "pass123", "E001", "C101", "Dev", "Axel");
+        Collaborator c2 = new Collaborator("Sara", "Ben", "sara@mail.com", "pass456", "E002", "C102", "Tester", "Yousra");
+        service.createCollaborator(c1);
+        service.createCollaborator(c2);
+        System.out.println("list of collaborators created");
+        service.showCollaborators();
+        service.deleteCollaborator(c1);
+        System.out.println("list of remaining collaborators");
 
     }
 }
