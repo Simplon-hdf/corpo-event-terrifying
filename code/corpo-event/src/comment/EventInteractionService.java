96c93177499fff9da9b71c94c8c7afff719e285c
Yousra0225 <chbibyousra02@gmail.com>
2025-05-24 21:46:13 +0200

    feat(eventinteraction-service): add create comment method

--- code/corpo-event/src/comment/EventInteractionService.java
+++ code/corpo-event/src/comment/EventInteractionService.java
@@ -4,10 +4,15 @@ import java.util.*;
 
 public class EventInteractionService {
     private List<EventInteraction> eventInteractions;
-    /** Constructor of the class **/
+    /** Constructor of the class */
     public EventInteractionService() { this.eventInteractions = new ArrayList<>(); }
 
+    /**  Method to create a comment */
     public void createComment(EventInteraction comment){
         this.eventInteractions.add(comment);
     }
+
+    
+
+
 }
