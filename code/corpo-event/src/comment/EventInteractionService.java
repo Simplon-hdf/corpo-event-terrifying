d1a97b444de2fc71659ec10bc00b5f816ff103df
Yousra0225 <chbibyousra02@gmail.com>
2025-05-24 21:54:10 +0200

    feat(eventinteraction-service): add show comment method to show all comments

--- code/corpo-event/src/comment/EventInteractionService.java
+++ code/corpo-event/src/comment/EventInteractionService.java
@@ -1,25 +1,48 @@
 package comment;
 
+import event.Event;
+
 import java.util.*;
 
 public class EventInteractionService {
-    private List<EventInteraction> eventInteractions;
+    private final List<EventInteraction> eventInteractions;
     /** Constructor of the class */
     public EventInteractionService() { this.eventInteractions = new ArrayList<>(); }
 
-    /**  Method to create a comment */
+    /**  Method to create a comment
+     * @param comment : a comment
+     */
     public void createComment(EventInteraction comment){
         this.eventInteractions.add(comment);
     }
 
-    /** Method to delete a comment */
+    /** Method to delete a comment
+     * @param comment : a comment
+     */
     public void deleteComment(EventInteraction comment){
         this.eventInteractions.remove(comment);
     }
 
-    /** Method to update a comment */
+    /** Method to update a comment
+     * @param comment : a comment
+     */
     public void updateComment(EventInteraction comment){
         int index = this.eventInteractions.indexOf(comment);
     }
+
+    public void showComment(EventInteraction comment){
+        System.out.println(comment.toString());
+    }
+
+    /**
+     * Method to show the list of comments
+     */
+    public void showComments(){
+        for (int i = 0; i < eventInteractions.size(); i++) {
+            System.out.println("------------");
+            showComment(eventInteractions.get(i));
+        }
+    }
+
 
 }
