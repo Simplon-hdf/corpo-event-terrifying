e3b99122f0db021cb312b2c6cac080d446ad5948
Yousra0225 <chbibyousra02@gmail.com>
2025-05-24 21:44:23 +0200

    feat(eventinteraction-service): add event interaction service service

---
+++ code/corpo-event/src/comment/EventInteractionService.java
@@ -0,0 +1,13 @@
+package comment;
+
+import java.util.*;
+
+public class EventInteractionService {
+    private List<EventInteraction> eventInteractions;
+    /** Constructor of the class **/
+    public EventInteractionService() { this.eventInteractions = new ArrayList<>(); }
+
+    public void createComment(EventInteraction comment){
+        this.eventInteractions.add(comment);
+    }
+}
